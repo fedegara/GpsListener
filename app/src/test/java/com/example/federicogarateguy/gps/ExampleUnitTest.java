c972f9f8908ece083a98bb22dfd65224a999bb57
Fedegara <fedegara91@gmail.com>
2018-01-22 14:55:40 -0300

    version 2

--- app/src/test/java/com/example/federicogarateguy/gps/ExampleUnitTest.java
+++ app/src/test/java/com/example/federicogarateguy/gps/ExampleUnitTest.java
@@ -11,8 +11,6 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
-        //prbando
-        //another comment
-        //comment for pablo
+        //version2
     }
 }
